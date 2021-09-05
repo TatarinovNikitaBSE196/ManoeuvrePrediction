6d43699f943f8847d6c53dd1530ba28d343270f5
Nikita Tatarinov <natatarinov@edu.hse.ru>
2021-09-05 13:31:54 +0300

    Seems ike final version

--- ML/5_learn.py
+++ ML/5_learn.py
@@ -4,11 +4,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn import tree
 import json
-import graphviz
 from sklearn2pmml.pipeline import PMMLPipeline
 from sklearn2pmml import sklearn2pmml
+
+import graphviz
+from sklearn import tree
 
 import settings
 
@@ -25,8 +26,10 @@ def run_once(dataset, filepath):
     print(f'Starting {dataset["dataset_name"]} {class_name}')
     Path(f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/').mkdir(parents=True, exist_ok=True)
     df = pd.read_csv(filepath)
-    df = df.drop(columns=['recordingId', 'frame', 'trackId', 'class', 'trackLifetime'])
+    df = df.drop(columns=['recordingId', 'frame', 'trackId', 'class'])
     print(df['prediction'].value_counts())
+    with open(f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/value_counts.txt', 'w') as f:
+        f.write(str(df['prediction'].value_counts()))
     x = df.drop('prediction', axis=1)
     y = df['prediction']
     y, prediction_names = pd.factorize(y, sort=True)
@@ -42,7 +45,6 @@ def run_once(dataset, filepath):
     accuracy = metrics.accuracy_score(y_train, y_train_pred)
     print("Accuracy: {:.2f}".format(accuracy))
     cm = confusion_matrix(y_train, y_train_pred)
-
     print('Confusion Matrix: \n', cm)
     with open(f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/train_cm.txt', 'w') as f:
         f.write(str(cm))
@@ -64,8 +66,8 @@ def run_once(dataset, filepath):
         f.write(str(cr))
 
     tree.export_graphviz(classifier, out_file=f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/graphviz.dot', filled=True, rounded=True, feature_names=x.columns, class_names=prediction_names)
-    # graph = graphviz.Source.from_file(f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/graphviz.dot')
-    # graph.render(f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/graphviz', format='png')
+    graph = graphviz.Source.from_file(f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/graphviz.dot')
+    graph.render(f'{settings.LEARNED_DATA_FOLDER}/{dataset["dataset_name"]}/{class_name}/graphviz', format='pdf')
 
     pipeline = PMMLPipeline([("classifier", classifier)])
     Path(f'{settings.TREES_FOLDER}/{dataset["dataset_name"]}/').mkdir(parents=True, exist_ok=True)
